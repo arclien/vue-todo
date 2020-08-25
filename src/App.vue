5eb2d0d21dbcc56506b0791a643074b628f38fb8
Jisuk Yoon <arclien>
2020-08-25 19:15:16 +0900

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-bind:todos="todos" v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
+import axios from "axios";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Header, Todos, AddTodo },
+  data() {
+    return {
+      todos: [],
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    async addTodo(todo) {
+      const { title, completed } = todo;
+      const { data } = await axios.post(
+        "https://jsonplaceholder.typicode.com/todos",
+        {
+          title,
+          completed,
+        }
+      );
+      this.todos = [...this.todos, data];
+    },
+  },
+  async created() {
+    const res = await axios.get(
+      "http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code=005930"
+    );
+    console.log(res);
+    // this.todos = data;
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="todo-item" v-bind:class="{'is-completed':todo.completed}">
+    <p>
+      <input type="checkbox" v-on:change="markComplete" />
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">X</button>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+  methods: {
+    markComplete() {
+      this.todo.completed = !this.todo.completed;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.todo-item {
+  background: #f4f4f4;
+  padding: 10px;
+  border-bottom: 1px dotted #ccc;
+}
+.is-completed {
+  text-decoration: line-through;
+}
+</style>
